017615b385b4bd5407585f1590748903ed41b437
RebelMerf <rebelmurf@gmail.com>
2017-02-28 14:37:42 -0500

    filled in the test and stubbed out the main vector_victor file

---
+++ vector_victor.py
@@ -0,0 +1,30 @@
+def shape():
+    pass
+
+
+def vector_add():
+    pass
+
+
+def vector_sub():
+    pass
+
+
+def vector_sum():
+    pass
+
+
+def dot():
+    pass
+
+
+def vector_multiply():
+    pass
+
+
+def vector_mean():
+    pass
+
+
+def magnitude():
+    pass
