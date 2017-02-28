02a96ced05ef809dd54430121af9ab951f59ad5a
RebelMerf <rebelmurf@gmail.com>
2017-02-28 15:41:24 -0500

    finally getting the feel of it, test driven, passed the first two tests so far

--- vector_victor.py
+++ vector_victor.py
@@ -1,9 +1,14 @@
-def shape():
-    pass
+def shape(thing):
+    n = len(thing)
+    #if type(thing[0]) == list:
+    #    k = len(thing[0])
+    #    return (n,k)
+    #else:
+    return (n,)
 
 
-def vector_add():
-    pass
+def vector_add(a, b):
+    return [a[i] + b[i] for i in range(len(a))]
 
 
 def vector_sub():
