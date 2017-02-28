8bcb65b1d5eaf3b8951aaa8ea2ad3d083710e6af
RebelMerf <rebelmurf@gmail.com>
2017-02-28 18:04:32 -0500

    yet more progress through the test driven development

--- vector_victor.py
+++ vector_victor.py
@@ -1,3 +1,7 @@
+class ShapeError(Exception):
+    pass
+
+
 def shape(thing):
     n = len(thing)
     #if type(thing[0]) == list:
@@ -7,24 +11,37 @@ def shape(thing):
     return (n,)
 
 
-def vector_add(a, b):
-    return [a[i] + b[i] for i in range(len(a))]
+def vector_add(v1, v2):
+    if shape(v1) == shape(v2):
+        return [a + b for a, b in zip(v1, v2)]
+    else:
+        raise ShapeError("Cannot add vectors of differing length")
 
 
-def vector_sub():
-    pass
+def vector_sub(v1, v2):
+    if shape(v1) == shape(v2):
+        return [a - b for a, b in zip(v1, v2)]
+    else:
+        raise ShapeError("Cannot subtract vectors of differing length")
 
 
-def vector_sum():
-    pass
+def vector_sum(*args):
+    comparisons = [len(arg) == len(args[0]) for arg in args]
+    if sum(comparisons) == len(comparisons):
+        return [sum(stuff) for stuff in zip(*args)]
+    else:
+        raise ShapeError("Cannot sum vectors of differing length")
 
 
-def dot():
-    pass
+def dot(v1, v2):
+    if shape(v1) == shape(v2):
+        return sum([a * b for a, b in zip(v1, v2)])
+    else:
+        raise ShapeError("Cannot dot vectors of differing length")
 
 
-def vector_multiply():
-    pass
+def vector_multiply(v, num):
+    return [a * num for a in v]
 
 
 def vector_mean():
